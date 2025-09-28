17def3249b8a028309dbab818de87f1232d7c699
ggkulanicisi <gameower260@gmail.com>
2025-09-28 19:13:44 +0300

    colors.js

---
+++ colors.js
@@ -0,0 +1,16 @@
+// colors.js
+
+// Arka plan renklerini bu dizi (array) içinde tutun.
+// Hex kodları (# ile başlayan) veya İngilizce renk isimleri kullanabilirsiniz.
+const RENKLER = [
+    "#ff6b81", // One için canlı kırmızı
+    "#48dbfb", // Two için parlak mavi
+    "#55e6c1", // Three için turkuaz
+    "#ffaf40", // Four için parlak turuncu
+    "#7d5fff", // Five için mor
+    "#f7d794", // Six için sarı
+    "#2ed573", // Eight için parlak yeşil (Seven atlandığı için)
+    "#ffa801", // Nine için altın sarısı
+    "#ff4757"  // Ten için koyu kırmızı
+    // (messages.js dosyanızda 9 kelime olduğu için 9 renk var)
+];
