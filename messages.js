e759921c92941f9c8035272d4b4f4ebf860aad0f
ggkulanicisi <gameower260@gmail.com>
2025-09-28 18:25:53 +0300

    messages.js

---
+++ messages.js
@@ -0,0 +1,15 @@
+// messages.js
+
+// Kelimelerinizi bu dizi (array) içinde tutun. 
+// Her kelime tırnak içinde ve virgülle ayrılmış olmalıdır.
+const KELIMELER = [
+    "One", 
+    "Two", 
+    "Three", 
+    "Four", 
+    "Five", 
+    "Six", 
+    "Eight", // "Seven" atlandı
+    "Nine", 
+    "Ten"
+];
